993b7a81e03a048f0aefdb4957fab76cf2525b4c
Md Al Amin <alaminrakib4864@gmail.com>
2026-02-24 15:30:04 +0600

    Add MinimumAbsoluteDifferenceInAnArray class with implementation of minimumAbsoluteDifference function

---
+++ src/HackerRank/MinimumAbsoluteDifferenceInAnArray.java
@@ -0,0 +1,61 @@
+package HackerRank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+// problem link: https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+class ResultMinimumAbsoluteDifference {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+        // Write your code here
+
+        int min = Integer.MAX_VALUE;
+        Collections.sort(arr);
+        for(int i = 0; i < arr.size()-1; i++){
+            int abs = Math.abs(arr.get(i) - arr.get(i+1));
+            if(abs < min) min = abs;
+        }
+
+        return  min;
+    }
+
+}
+
+public class MinimumAbsoluteDifferenceInAnArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = ResultMinimumAbsoluteDifference.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
